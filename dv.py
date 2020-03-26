038bdd7b9e9405b7880a30d638ffab2a5c9571ee
ray1076027 <59588574+ray1076027@users.noreply.github.com>
2020-03-26 12:36:47 +0800

    DS part
    新增digital signature

--- dv.py
+++ dv.py
@@ -15,7 +15,9 @@ class HMAC:
         #紀錄密鑰
         with open('HMAC_key.pem','wb') as f:
             f.write(self.key)
-        return tag
+        return tag,self.key
+    def get_key(self):
+        return self.key
 
 class PKE:
     def __init__(self,msg):
@@ -23,14 +25,14 @@ class PKE:
         #產生rsa pk,sk
         self.sk = rsa.generate_private_key(
             public_exponent=65537,
-            key_size=1024,
+            key_size=2048,
             backend=default_backend()
         )
         self.pk = self.sk.public_key()
 
     def encrypt(self):
         #加密
-        ciphertext = self.pk.encrypt(
+        self.ciphertext = self.pk.encrypt(
             self.msg,
             padding.OAEP(
                 mgf=padding.MGF1(algorithm=hashes.SHA256()),
@@ -38,10 +40,65 @@ class PKE:
                 label=None
             )
         )
-        return ciphertext
+        return self.ciphertext
+
+    def decrypt(self,ciphertext):
+        #解密
+        self.ciphertext = ciphertext
+        self.plaintext = self.sk.decrypt(
+            self.ciphertext,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        return self.plaintext.decode('utf-8')
+
+class DS:
+    def __init__(self,msg):
+        self.msg = bytes(msg,'utf-8')
+        self.sk = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+            backend=default_backend()
+        )
+        self.pk = self.sk.public_key()
+
+    def sign(self):
+        self.signature = self.sk.sign(
+            self.msg,
+            padding.PSS(
+                mgf = padding.MGF1(hashes.SHA256()),
+                salt_length = padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        return self.signature
+
+    def verify(self, signature):
+        self.signature = signature
+        self.vrfy = self.pk.verify(
+            self.signature,
+            self.msg,
+            padding.PSS(
+                mgf = padding.MGF1(hashes.SHA256()),
+                salt_length = padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        return self.vrfy
 
 if __name__ == '__main__':
     msg = 'hellow'
-    tag = HMAC(msg).mac()
-    ciphertext = PKE(msg).encrypt().hex()
-    print(tag,ciphertext)
+    tag,mac_key = HMAC(msg).mac()
+    pke = PKE(mac_key.hex())
+    ciphertext = pke.encrypt()
+    plaintext = pke.decrypt(ciphertext)
+    #print(mac_key.hex() == plaintext)#check plaintext
+    #print(tag)
+    ds = DS(msg)
+    sigma = ds.sign()
+    vrfy = ds.verify(sigma)
+    print(sigma.hex())
+    print(vrfy)
